6c50357393bc6dbb8a9dfc7dbc993bfd157e75e2
porte <porte@LAPTOP-D41JJJ1I>
2022-10-24 11:16:20 -0600

    Started app Put in packages, db, jsp, servlet. Working on data access.

---
+++ src/java/dataaccess/UserDB.java
@@ -0,0 +1,130 @@
+package dataaccess;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import models.User;
+
+public class UserDB {
+
+    public List<User> getAll(String owner) throws Exception {
+        List<User> users = new ArrayList<>();
+        ConnectionPool cp = ConnectionPool.getInstance();
+        Connection con = cp.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        
+        String sql = "SELECT * FROM userdb.user";
+        
+        try {
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                
+                String userEmail = rs.getString(1);
+                String userFn = rs.getString(2);
+                String userLn = rs.getString(3);
+                String userPassword = rs.getString(4);
+                int userRole = rs.getInt(5);
+                
+                User user = new User(userEmail, userFn, userLn, userPassword, userRole);
+                users.add(user);
+            }
+        } finally {
+            DBUtil.closeResultSet(rs);
+            DBUtil.closePreparedStatement(ps);
+            cp.freeConnection(con);
+        }
+
+        return users;
+    }
+
+    public User get(String email) throws Exception {
+        User user = null;
+        
+        ConnectionPool cp = ConnectionPool.getInstance();
+        Connection con = cp.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        
+        String sql = "SELECT * FROM userdb.user WHERE email=\"?\"";
+        
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, email);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                String userEmail = rs.getString(1);
+                String userFn = rs.getString(2);
+                String userLn = rs.getString(3);
+                String userPassword = rs.getString(4);
+                int userRole = rs.getInt(5);
+                
+                user = new User(userEmail, userFn, userLn, userPassword, userRole);
+            }
+        } finally {
+            DBUtil.closeResultSet(rs);
+            DBUtil.closePreparedStatement(ps);
+            cp.freeConnection(con);
+        }
+        
+        return user;
+    }
+
+//    below methods need properly implemented
+    
+    public void insert(Note note) throws Exception {
+        ConnectionPool cp = ConnectionPool.getInstance();
+        Connection con = cp.getConnection();
+        PreparedStatement ps = null;
+        String sql = "INSERT INTO note (title, contents, owner) VALUES (?, ?, ?)";
+        
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, note.getTitle());
+            ps.setString(2, note.getContents());
+            ps.setString(3, note.getOwner());
+            ps.executeUpdate();
+        } finally {
+            DBUtil.closePreparedStatement(ps);
+            cp.freeConnection(con);
+        }
+    }
+
+    public void update(Note note) throws Exception {
+        ConnectionPool cp = ConnectionPool.getInstance();
+        Connection con = cp.getConnection();
+        PreparedStatement ps = null;
+        String sql = "UPDATE note SET title=?, contents=? WHERE note_id=?";
+        
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, note.getTitle());
+            ps.setString(2, note.getContents());
+            ps.setInt(3, note.getNoteId());
+            ps.executeUpdate();
+        } finally {
+            DBUtil.closePreparedStatement(ps);
+            cp.freeConnection(con);
+        }
+    }
+
+    public void delete(Note note) throws Exception {
+        ConnectionPool cp = ConnectionPool.getInstance();
+        Connection con = cp.getConnection();
+        PreparedStatement ps = null;
+        String sql = "DELETE FROM note WHERE note_id=?";
+        
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, note.getNoteId());
+            ps.executeUpdate();
+        } finally {
+            DBUtil.closePreparedStatement(ps);
+            cp.freeConnection(con);
+        }
+    }
+
+}
