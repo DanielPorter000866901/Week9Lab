1c00a2a139973fc33efb5610f849db5da8527dce
porte <porte@LAPTOP-D41JJJ1I>
2022-11-03 11:36:48 -0600

    Ripped up UserDB and RoleDB and started replacing with JPA. Also replaced models with JPA entities.

--- src/java/dataaccess/UserDB.java
+++ src/java/dataaccess/UserDB.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.EntityManager;
 import models.User;
 import models.Role;
 
@@ -20,36 +21,14 @@ public class UserDB {
      * @throws Exception 
      */
     public List<User> getAll() throws Exception {
-        List<User> users = new ArrayList<>();
-        ConnectionPool cp = ConnectionPool.getInstance();
-        Connection con = cp.getConnection();
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        
-        String sql = "SELECT * FROM userdb.user";
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
         
         try {
-            ps = con.prepareStatement(sql);
-            rs = ps.executeQuery();
-            while (rs.next()) {
-                
-                String userEmail = rs.getString(1);
-                String userFn = rs.getString(2);
-                String userLn = rs.getString(3);
-                String userPassword = rs.getString(4);
-                int userRole = rs.getInt(5);
-                
-                // the role at this time is only the role id given in the table
-                User user = new User(userEmail, userFn, userLn, userPassword, new Role(userRole, ""));
-                users.add(user);
-            }
+            List<User> users = em.createNamedQuery("User.findAll", User.class).getResultList();
+            return users;
         } finally {
-            DBUtil.closeResultSet(rs);
-            DBUtil.closePreparedStatement(ps);
-            cp.freeConnection(con);
+            em.close();
         }
-
-        return users;
     }
 
     /**
@@ -59,38 +38,14 @@ public class UserDB {
      * @throws Exception 
      */
     public User get(String email) throws Exception {
-        User user = null;
-        
-        ConnectionPool cp = ConnectionPool.getInstance();
-        Connection con = cp.getConnection();
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        
-        String sql = "SELECT * FROM userdb.user WHERE email=?";
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
         
         try {
-            ps = con.prepareStatement(sql);
-            
-            ps.setString(1, email);
-            rs = ps.executeQuery();
-            
-            if (rs.next()) {
-                String userEmail = rs.getString(1);
-                String userFn = rs.getString(2);
-                String userLn = rs.getString(3);
-                String userPassword = rs.getString(4);
-                int userRole = rs.getInt(5);
-                
-                // the role at this time is only the role id given in the table
-                user = new User(userEmail, userFn, userLn, userPassword, new Role(userRole, ""));
-            }
+            User user = em.find(User.class, email);
+            return user;
         } finally {
-            DBUtil.closeResultSet(rs);
-            DBUtil.closePreparedStatement(ps);
-            cp.freeConnection(con);
+            em.close();
         }
-        
-        return user;
     }
     
     /**
@@ -99,24 +54,12 @@ public class UserDB {
      * @throws Exception 
      */
     public void insert(User user) throws Exception {
-        ConnectionPool cp = ConnectionPool.getInstance();
-        Connection con = cp.getConnection();
-        PreparedStatement ps = null;
-        String sql = "INSERT INTO userdb.user (email, first_name, last_name, password, role) VALUES (?, ?, ?, ?, ?)";
+        
         
         try {
-            ps = con.prepareStatement(sql);
             
-            ps.setString(1, user.getEmail());
-            ps.setString(2, user.getFirstName());
-            ps.setString(3, user.getLastName());
-            ps.setString(4, user.getPassword());
-            ps.setInt(5, user.getRole().getId());
-            
-            ps.executeUpdate();
         } finally {
-            DBUtil.closePreparedStatement(ps);
-            cp.freeConnection(con);
+            
         }
     }
 
@@ -126,25 +69,12 @@ public class UserDB {
      * @throws Exception 
      */
     public void update(User user) throws Exception {
-        ConnectionPool cp = ConnectionPool.getInstance();
-        Connection con = cp.getConnection();
-        PreparedStatement ps = null;
-        String sql = "UPDATE userdb.user SET first_name=?, last_name=?, password=?, role=? WHERE email=?";
+        
         
         try {
-            ps = con.prepareStatement(sql);
             
-            ps.setString(1, user.getFirstName());
-            ps.setString(2, user.getLastName());
-            ps.setString(3, user.getPassword());
-            ps.setInt(4, user.getRole().getId());
-            
-            ps.setString(5, user.getEmail());
-            
-            ps.executeUpdate();
         } finally {
-            DBUtil.closePreparedStatement(ps);
-            cp.freeConnection(con);
+            
         }
     }
 
@@ -154,18 +84,12 @@ public class UserDB {
      * @throws Exception 
      */
     public void delete(String email) throws Exception {
-        ConnectionPool cp = ConnectionPool.getInstance();
-        Connection con = cp.getConnection();
-        PreparedStatement ps = null;
-        String sql = "DELETE FROM userdb.user WHERE email=(?)";
+        
         
         try {
-            ps = con.prepareStatement(sql);
-            ps.setString(1, email);
-            ps.executeUpdate();
+            
         } finally {
-            DBUtil.closePreparedStatement(ps);
-            cp.freeConnection(con);
+            
         }
     }
 
